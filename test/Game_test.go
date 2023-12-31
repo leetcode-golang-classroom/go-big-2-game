6b2eeecfda8d821d43b85d8135716b2151540e7d
gson Liang <yuanyu90221@gmail.com>
2024-01-01 03:03:45 +0800

    feat(add test for normal work): add test for normal work

--- test/Game_test.go
+++ test/Game_test.go
@@ -157,3 +157,77 @@ func TestWithIllegalActions(t *testing.T) {
 		t.Errorf("expected: %v, got: %v", expected, got)
 	}
 }
+
+func TestWithNormalNoErrorPlay1(t *testing.T) {
+	var b bytes.Buffer
+	absPath, _ := filepath.Abs("../test_cases/normal-no-error-play1.in")
+	absOutputPath, _ := filepath.Abs("../test_cases/normal-no-error-play1.out")
+	file, _ := os.Open(absPath)
+	outputFile, _ := os.ReadFile(absOutputPath)
+	ioReader := bufio.NewReader(file)
+	ioWriter := bufio.NewWriter(&b)
+	cardPatternHdr := big_2_game.NewSingleCardPatternHandelr(
+		big_2_game.NewPairCardPatternHandler(
+			big_2_game.NewStraightCardPatternHandler(
+				big_2_game.NewFullHouseCardPatternHandler(
+					nil,
+				),
+			),
+		),
+	)
+
+	big2Game := big_2_game.NewGame(cardPatternHdr, []big_2_game.PlayerInterface{
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+	},
+		big_2_game.NewDeck(),
+		ioReader,
+		big_2_game.NewShuffleFromFileStragtegy(),
+		ioWriter,
+	)
+	big2Game.GameFlow()
+	got := b.String()
+	expected := string(outputFile)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected: %v, got: %v", expected, got)
+	}
+}
+
+func TestWithNormalNoErrorPlay2(t *testing.T) {
+	var b bytes.Buffer
+	absPath, _ := filepath.Abs("../test_cases/normal-no-error-play2.in")
+	absOutputPath, _ := filepath.Abs("../test_cases/normal-no-error-play2.out")
+	file, _ := os.Open(absPath)
+	outputFile, _ := os.ReadFile(absOutputPath)
+	ioReader := bufio.NewReader(file)
+	ioWriter := bufio.NewWriter(&b)
+	cardPatternHdr := big_2_game.NewSingleCardPatternHandelr(
+		big_2_game.NewPairCardPatternHandler(
+			big_2_game.NewStraightCardPatternHandler(
+				big_2_game.NewFullHouseCardPatternHandler(
+					nil,
+				),
+			),
+		),
+	)
+
+	big2Game := big_2_game.NewGame(cardPatternHdr, []big_2_game.PlayerInterface{
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+		big_2_game.NewHumanPlayer(ioReader, ioWriter),
+	},
+		big_2_game.NewDeck(),
+		ioReader,
+		big_2_game.NewShuffleFromFileStragtegy(),
+		ioWriter,
+	)
+	big2Game.GameFlow()
+	got := b.String()
+	expected := string(outputFile)
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("expected: %v, got: %v", expected, got)
+	}
+}
